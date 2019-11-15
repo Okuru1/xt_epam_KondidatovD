e33ffb723d47460e6233d400563a168527da619b
okuru1 <okuru1@bk.ru>
2019-11-15 14:52:52 +0400

    Add solution for task0.1 SEQUENCE

--- xt_epam_KondidatovD/task0.1/source 01.cs
+++ xt_epam_KondidatovD/task0.1/source 01.cs
@@ -4,9 +4,17 @@ namespace task0._1
 {
     class Program
     {
+        static void Function(int n)
+        {
+            for (int i = 0; i <= n; i++)
+                Console.Write("{0} ", i);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter N");
+            Function(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadKey();
         }
     }
 }
