6a87305e367df0a5c13d2b97dc771e2c628b1ea5
okuru1 <okuru1@bk.ru>
2019-11-23 19:38:48 +0400

    Task 01 completed in full

--- xt_epam_Task01_KondidatovD/task1.11AverageStringLength/task1.11.cs
+++ xt_epam_Task01_KondidatovD/task1.11AverageStringLength/task1.11.cs
@@ -11,11 +11,49 @@ namespace task1_11
         public static void Main()
         {
             string s;
+            string[] arrayOfStrings;
+            int average = 0;
             Console.WriteLine("Task 1.11 for XT_EPAM" + "\n\r--------------------");
             s = Console.ReadLine();
-            //Console.WriteLine(s);
-            string[] str = s.Split();       
-        
+            StringBuilder str = new StringBuilder(s); 
+            RemovePunctuation(ref str, true); //Удаляем знаки пунктуации 
+            s = str.ToString();
+            arrayOfStrings = s.Split(null as string[], StringSplitOptions.RemoveEmptyEntries);           
+            if (arrayOfStrings.Length - 1 != 0)
+            {
+                for (int i = 0; i < arrayOfStrings.Length; i++)
+                {
+                    if (arrayOfStrings[i].Length != 0)
+                    {
+                        Console.Write($"{i}) {arrayOfStrings[i]}");
+                        Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+                        Console.Write($"    [the length of string is { arrayOfStrings[i].Length }]\n\r");
+                        Console.ResetColor(); // сбрасываем в стандартный                                         
+                        average += arrayOfStrings[i].Length;
+                    }
+                }
+                average /= (arrayOfStrings.Length);
+                Console.WriteLine($"Аverage word length per line is {average}");
+            }
+            else
+            {
+                Console.WriteLine("You haven't enter the word");
+            }
+        }
+
+        static void RemovePunctuation(ref StringBuilder inputString, bool inConsole = false)
+        {
+            for (int i = inputString.Length - 1; i >= 0; i--)
+                if (char.IsPunctuation(inputString[i]))
+                    inputString.Remove(i, 1);
+            if (inConsole)
+            {
+                Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+                Console.Write("All punctuation has removed \n\r");
+                Console.ResetColor(); // сбрасываем в стандартный              
+                Console.Write(inputString + "\n\r");
+            }
         }
     }
 }
+
--- xt_epam_Task01_KondidatovD/task1.12CharDoubler/task1.12.cs
+++ xt_epam_Task01_KondidatovD/task1.12CharDoubler/task1.12.cs
@@ -11,10 +11,31 @@ namespace task1_12
         public static void Main()
         {
             Console.WriteLine("Task 1.12 for XT_EPAM" + "\n\r--------------------");
+            string inputOne;
+            string inputTwo;
+            Console.WriteLine("Enter the first line: ");
+            inputOne = Console.ReadLine();
+            Console.WriteLine("Enter the second line: ");
+            inputTwo = Console.ReadLine();
+            string doubled = CharDoubler(inputOne,inputTwo);
+            Console.WriteLine("Result: ");
+            Console.Write(doubled);
+        }
 
-
-
-
+        static string CharDoubler(string mainString, string addString)
+        {
+            StringBuilder result =new StringBuilder();
+            for (int i = 0; i < mainString.Length; i++)
+            {
+                result.Append(mainString[i]);
+                for (int j = 0; j < addString.Length; j++)
+                    if (mainString[i] == addString[j])
+                    {
+                        result.Append(addString[j]);
+                        break;
+                    }             
+            }
+            return result.ToString();
         }
     }
 }
